d36f9c621d85dd8131717d6ff23d061cb30c359d
jihaddupuy <jihad.dupuy@gmail.com>
2020-11-02 09:58:58 -0500

    added getDount jasmine test

--- spec/js/Donutmaker.spec.js
+++ spec/js/Donutmaker.spec.js
@@ -3,7 +3,7 @@ describe('donut maker should add donuts:', function(){
     let underTest;
     
     beforeEach( function(){
-        underTest = new DonutMaker();
+        underTest = new DonutMaker()
     })
 
    describe('donut maker should have properties', function(){
@@ -11,6 +11,11 @@ describe('donut maker should add donuts:', function(){
         describe('should have donut', function(){
             it('should have a default of 0',function(){
             expect(underTest.donut).toBe(0)
+            })
+        })
+        describe('get donut should return current value of donut',function(){
+            it('should retun 0 for new car',function(){
+                expect(underTest.getDonut()).toBe(0)
             })
         })
    })
