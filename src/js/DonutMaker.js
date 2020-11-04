bde86f8c59bc7cd47eb1cdb5e106581ee540a187
jihaddupuy <jihad.dupuy@gmail.com>
2020-11-04 10:01:11 -0500

    added increase auto clicker cost method

--- src/js/DonutMaker.js
+++ src/js/DonutMaker.js
@@ -2,23 +2,26 @@ class DonutMaker {
     constructor(){
         this.donutCount = 0;
         this.autoClickerCount=0;
-        this.outClickerCost=100;
-    }
-    
-    getDonut(){
-        return this.donutCount;
+        this.autoClickerCost=100;
     }
     addDonut(){
         this.donutCount += 1;
     }
+    getDonut(){
+        return this.donutCount;
+    }
+   
     purchaseAutoClicker(){
         this.autoClickerCount+=1;
     }
     buyAutoClicker(){
-         this.donut++
+         this.donutCount++
         }
     getAutoClickerCount(){
         return this.autoClickerCount;
+    }
+    IncreaseCostOfAutoClicker(){
+        this.autoClickerCost +=0.10;
     }
 }
 
