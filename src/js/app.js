18e2235fa2836c094bc38ef79253f095b3a969e0
jihaddupuy <jihad.dupuy@gmail.com>
2020-11-06 14:54:37 -0500

    made donutClick funtional

--- src/js/app.js
+++ src/js/app.js
@@ -3,21 +3,35 @@ const purchaseAutoClickerElement = document.querySelector('.autoClicker_Dount_Di
 const purchaseDonutMutiplierElement = document.querySelector('.donut_Mutiplier_Display');
 
 const addDonutClickButton = document.querySelector('.donut_Click_button')
-const purchaseAutoClickerButton = document.querySelector('.purchase_Auto_Clicker-Button')
-const purchaseDonutMutiplier = document.querySelector('.purchase_Donut_Multipler')
+//const purchaseAutoClickerButton = document.querySelector('.purchase_Auto_Clicker-Button')
+//const purchaseDonutMutiplier = document.querySelector('.purchase_Donut_Multipler')
 
 const donutMaker = new DonutMaker();
 
-let isDonutCounterActive = false;
-let intervaralId =null;
+//let isDonutCounterActive = false;
+//let intervaralId =null;
 
 // update donut count Display
-const updateDonutCountDisplay =function(){
+const updateDonutCountDisplay = function(){
     donutCountElement.innerText = donutMaker.getDonutCount();
 }
 
 // update auto click count display
-const UpdateAutoClickerCount = function(){
+const updateAutoClickerCount = function(){
  purchaseAutoClickerElement.innerText = donutMaker.purchaseAutoClicker();
 }
 
+// update donut Multipler count display
+const updateDonutMultipler = function(){
+    purchaseDonutMutiplierElement.innerText = donutMaker.getDonutMultiplier();
+}
+
+// add eventListener to Donut Click button
+addDonutClickButton.addEventListener("click",function(){
+    donutMaker.addDonut();
+    
+    updateDonutCountDisplay();
+
+})
+
+updateDonutCountDisplay();
