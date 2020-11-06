862dc765a63706a8da88b57bb3bdc9969cf82b27
jihaddupuy <jihad.dupuy@gmail.com>
2020-11-06 13:36:05 -0500

    refactored index html interface

---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+const donutCountElement = document.querySelector('.donut_Count_Display');
+const purchaseAutoClickerElement = document.querySelector('.autoClicker_Dount_Display');
+const purchaseDonutMutiplierElement = document.querySelector('.donut_Mutiplier_Display');
+
+const addDonutClickButton = document.querySelector('.donut_Click_button')
+const purchaseAutoClickerButton = document.querySelector('.purchase_Auto_Clicker-Button')
+const purchaseDonutMutiplier = document.querySelector('.purchase_Donut_Multipler')
+
+const donutMaker = new DonutMaker();
+
+let isDonutCounterActive = false;
+let intervaralId =null;
+
+// update donut count Display
+const updateDonutCountDisplay =function(){
+    donutCountElement.innerText = donutMaker.getDonutCount();
+}
+
+// update auto click count display
+const UpdateAutoClickerCount = function(){
+ purchaseAutoClickerElement.innerText = donutMaker.purchaseAutoClicker();
+}
+
